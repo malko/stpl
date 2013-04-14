beaea3b0bc410f8f00fc79fd033b1e49d56646de
malko <jgotti@jgotti.org>
2013-04-14 23:39:05 +0200

    make it also a node module

---
+++ gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/* (c) Jonathan Gotti - licence: https://github.com/malko/stpl/LICENCE.txt */\n'
+      },
+      build: {
+        src: 'lib/stpl.js',
+        dest: 'lib/stpl.min.js'
+      }
+    },
+    qunit: {
+      all: ['tests/*.html']
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  
+  // Default task(s).
+  grunt.registerTask('default', ['uglify','qunit']);
+
+};
